53eccaf8ee57e1893f2ed37d016e4d9f87602ff5
wanggen <576192941@qq.com>
2015-03-25 14:55:30 +0800

    project init

---
+++ README.md
@@ -0,0 +1,2 @@
+
+> simple web-app based on python
--- daos-web.py
+++ daos-web.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from flask import Flask, json
+from flask import Flask
 from jinja2 import Environment, PackageLoader
 from cassandra.cluster import Cluster
 import logging
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 env = Environment(loader=PackageLoader('daos-web', '.'))
 
-cluster = Cluster(contact_points=["wg-linux"])
+cluster = Cluster(contact_points=["10.0.0.8"])
 session = cluster.connect("groupon")
 
 @app.route('/')
@@ -18,34 +18,52 @@ def index():
 
 
 @app.route('/dealsummary')
-def summary():
-    try:
-        page = env.get_template("components/dealsummary/view.html")\
-            .render(data=do_query(), component='dealsummary')
-    except:
-        logging.exception("error")
-        raise
-    return page
-    # return json.jsonify({'data': do_query()})
+def dealsummary():
+    return env.get_template("components/dealsummary/view.html")\
+            .render(data=do_query_dealsummary(), component='dealsummary')
 
-
-def do_query():
+def do_query_dealsummary():
     try:
         data_list = []
         rows = session.execute("SELECT * from groupon_summary_deals")
         for row in rows:
             record = {
-                'sum_for':      row.sum_for.strftime('%Y-%m-%d'),
-                'deal':         row.deal/100,
-                'deal_item':    row.deal_item,
-                'deal_order':   row.deal_order,
-                'gmv':          row.gmv,
-                'gross_item':   row.gross_item,
-                'gross_order':  row.gross_order,
-                'per_order':    row.per_order
+                'sum_for':     row.sum_for.strftime('%Y-%m-%d'),
+                'deal':        row.deal/100,
+                'deal_item':   row.deal_item,
+                'deal_order':  row.deal_order,
+                'gmv':         row.gmv,
+                'gross_item':  row.gross_item,
+                'gross_order': row.gross_order,
+                'per_order':   row.per_order
             }
             data_list.append(record)
         data_list.sort(key=lambda r: r['sum_for'], reverse=True)
+        return data_list
+    except:
+        logging.error("未知异常")
+        raise
+
+
+@app.route('/usersummary')
+def usersummary():
+    return env.get_template("components/usersummary/view.html") \
+            .render(data=do_query_usersummary(), component='usersummary')
+
+
+def do_query_usersummary():
+    try:
+        data_list = []
+        rows = session.execute("SELECT * from groupon_summary_users")
+        for row in rows:
+            record = {
+                'date':  str(row.year)+"-"+str(row.month)+"-"+str(row.date),
+                'from':  row.channel,
+                'incr':  row.net_increase,
+                'total': row.total
+            }
+            data_list.append(record)
+        data_list.sort(key=lambda r: r['date'], reverse=True)
         return data_list
     except:
         logging.error("未知异常")
